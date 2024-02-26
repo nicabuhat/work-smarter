345fac9c0a80bfece4990547f0a24a8051fafd66
nica-cabuhat <nicaela.cabuhat@sophos.com>
2024-02-27 04:51:12 +0800

    FEAT: category distinction

--- app/(pages)/api/subcategory/route.tsx
+++ app/(pages)/api/subcategory/route.tsx
@@ -2,7 +2,20 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
 
 export async function GET(req: NextRequest) {
-  const subcategory = await prisma.subcategory.findMany();
+  const url = new URL(req.url);
+  const category = parseInt(url.searchParams.get("category")!);
+  let subcategory;
+
+  console.log(category);
+  if (category === 3) {
+    subcategory = await prisma.subcategory.findMany({
+      where: {
+        id: {
+          lt: 16,
+        },
+      },
+    });
+  } else subcategory = await prisma.subcategory.findMany();
 
   return NextResponse.json(subcategory);
 }
--- app/components/Column/SubCategory.tsx
+++ app/components/Column/SubCategory.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { ChangeEvent, useEffect, useState, useRef } from "react";
-import { useAppDispatch } from "@/redux/hooks";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { updateCategory } from "@/redux/features/columnsSlice";
 import Dropdown from "@/components/Dropdown";
 import Category from "@/models/Category";
@@ -8,28 +8,14 @@ import { handleClickOutside } from "@/util/utilities";
 
 export default function SubCategory({ id }: { id: number }): JSX.Element {
   const dispatch = useAppDispatch();
-
+  const columns = useAppSelector((state) => state.columnsReducer);
+  const currentColumn = columns[columns.findIndex((c) => c.id === id)];
   const [categories, setCategories] = useState([] as Category[]);
   const [inputValue, setInputValue] = useState("" as string);
   const [searchList, setSearchList] = useState([] as Category[]);
   const [checked, setChecked] = useState(false);
 
   const componentRef = useRef(null);
-
-  const getCategoriessArray = async () => {
-    try {
-      const res = await fetch("/api/subcategory");
-      const categories = res.json();
-
-      if (!res.ok) {
-        throw new Error("Failed to fetch categories");
-      }
-
-      return categories;
-    } catch (error) {
-      console.error("Catch Error:", error);
-    }
-  };
 
   const handleChange = (event: ChangeEvent) => {
     setInputValue((event.target as HTMLInputElement).value);
@@ -48,12 +34,29 @@ export default function SubCategory({ id }: { id: number }): JSX.Element {
   };
 
   useEffect(() => {
+    const getCategoriessArray = async () => {
+      try {
+        const res = await fetch(
+          `/api/subcategory?category=${currentColumn.parent_category?.id}`
+        );
+        const categories = res.json();
+
+        if (!res.ok) {
+          throw new Error("Failed to fetch categories");
+        }
+
+        return categories;
+      } catch (error) {
+        console.error("Catch Error:", error);
+      }
+    };
+
     getCategoriessArray().then((res: Category[]) => {
       res
         ? setCategories(res.sort((a, b) => a.name.localeCompare(b.name)))
         : setCategories([]);
     });
-  }, []);
+  }, [currentColumn]);
 
   useEffect(() => {
     setSearchList(
